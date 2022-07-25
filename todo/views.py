170c10d4a50c070d2d1ce8d52223fe7a70ef3d0a
Stuart Roeszler <stuart@roeszler.com>
2022-07-25 08:28:28 +0000

    add django templates funcitonality

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import (
+    render
+    # HttpResponse
+    )
 
 # Create your views here.
-def say_hello(request):
-    return HttpResponse("Hello!")
+# def say_hello(request):
+    # return HttpResponse("<h1>Hello!</h1><p>This is a paragraph.</p)>"
+def get_todo_list(request):
+    return render(request, "todo/todo_list.html")
