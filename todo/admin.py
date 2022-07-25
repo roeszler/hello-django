db1283ea575f7be7cb812138fc22e80be0b02622
Stuart Roeszler <stuart@roeszler.com>
2022-07-25 11:45:31 +0000

    add rendering data to .html via views.py

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,8 @@
+"""
+Import Modules
+"""
 from django.contrib import admin
+from .models import Item  # imports the item 'class'
 
 # Register your models here.
+admin.site.register(Item)
--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,27 @@
+"""
+Import Modules
+"""
 from django.shortcuts import (
     render
     # HttpResponse
     )
+from .models import Item
 
 # Create your views here.
 # def say_hello(request):
-    # return HttpResponse("<h1>Hello!</h1><p>This is a paragraph.</p)>"
+# return HttpResponse("<h1>Hello!</h1><p>This is a paragraph.</p)>"
+
+
 def get_todo_list(request):
-    return render(request, "todo/todo_list.html")
+    """
+    Define landing page.
+    Ensure communication between the front end users and our
+    database on the back end.
+    Add context as a third argument to the render function to
+    ensure we have access to it in our todo_list.html template.
+    """
+    items = Item.objects.all()  # import all items from db
+    context = {  # define a dictionary with all our imported items in it
+        'items': items
+    }
+    return render(request, "todo/todo_list.html", context)
